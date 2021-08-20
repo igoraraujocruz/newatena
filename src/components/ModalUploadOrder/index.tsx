307bff4575363517cfcd14a71e5593e880c9fcf1
Igor Araujo Cruz <igoraraujocruzz@gmail.com>
2021-08-20 16:28:42 -0300

    att

--- src/components/ModalUploadOrder/index.tsx
+++ src/components/ModalUploadOrder/index.tsx
@@ -2,10 +2,12 @@ import Modal from 'react-modal';
 import closeImg from '../../assets/close.svg'
 import { Form } from '@unform/web';
 import { ChangeEvent, useCallback, useState, useRef } from 'react';
+import { useToast } from '../../hooks/useToast';
 import Button from '../Button';
 import { useAuth } from '../../hooks/useAuth';
 import {api} from '../../services/api'
 import { FormHandles } from '@unform/core';
+import Input from '../Input';
 
 interface Order {
   id: string,
@@ -36,8 +38,10 @@ interface ModalEditOrderProps {
 
 export function ModalUploadOrder({isOpen, onRequestClose, currentOrder}: ModalEditOrderProps) {
   const { user } = useAuth()
+  const { addToast } = useToast();
   const formRef = useRef<FormHandles>(null);
   const [file, setFile] = useState({} as File);
+  const [name, setName] = useState('');
 
   const handleFile = useCallback(
     (e: ChangeEvent<HTMLInputElement>) => {
@@ -51,16 +55,28 @@ export function ModalUploadOrder({isOpen, onRequestClose, currentOrder}: ModalEd
   );
 
   const handleUpload = useCallback(() => {
-
       const data = new FormData();
       data.append('file', file)
-      data.append("name", 'teste')
+      data.append("name", name)
       data.append("order_id", currentOrder.id)
       data.append("user_id", user.id)
 
       api.post('/orders/upload/', data)
+
+      api.post('/orders/history/', {
+        message: `${user.name} Anexou o arquivo ${name}`,
+        order_id: currentOrder.id,
+        user_id: user.id
+       })
+
+       addToast({
+        type: 'success',
+        title: 'Arquivo enviado com sucesso',
+      }) 
+
+      onRequestClose()
     },
-    [currentOrder, file, user.id],
+    [currentOrder, file, user.id, addToast, onRequestClose, user.name, name],
   );
 
     return (
@@ -74,13 +90,20 @@ export function ModalUploadOrder({isOpen, onRequestClose, currentOrder}: ModalEd
         </button>    
         <Form ref={formRef} onSubmit={handleUpload}>
             <h2>Importar documentos</h2>
-            
+
+            <Input
+            name="name"
+            type='text'
+            id='name'
+            placeholder="Escreva o nome do arquivo"
+            onChange={event => setName(event.target.value)}
+            />            
             <input
             type='file'
             id='document'
             onChange={handleFile}
           />
-            <Button type="submit">Confirmar Edição</Button>
+            <Button type="submit">Enviar Arquivo</Button>
         </Form>
         </Modal>
     )
